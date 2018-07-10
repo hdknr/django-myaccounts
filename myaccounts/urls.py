998e69f4980f4788de1937a159d10536feee87f2
hdknr <gmail@hdknr.com>
2018-07-10 10:59:17 +0900

    django 2.0 / not bootstrap3

--- myaccounts/urls.py
+++ myaccounts/urls.py
@@ -1,16 +1,13 @@
-# coding: utf-8
-from django.conf.urls import url, include
+from django.urls import path, include
 from . import views, api
 
 
 # TODO: profile api: configurable
 
 urlpatterns = [
-    url(r'^social/',
-        include('social.apps.django_app.urls', namespace='social')),
-    url(r'^oauth2/api/profile', api.profile, name="api_profile"),
-    url(r'^oauth2/',
-        include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(r'^login', views.login, name="login"),
-    url(r'^logout', views.logout, name="logout"),
+    path('social/', include('social.apps.django_app.urls', namespace='social')),
+    path('oauth2/api/profile', api.profile, name="api_profile"),
+    path('oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
 ]
